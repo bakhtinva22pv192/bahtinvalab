bd4c2c22d32156dc9527ef374fff8514fa4d6cff
bakhtinva22pv192 <113528355+bakhtinva22pv192@users.noreply.github.com>
2022-12-27 19:42:21 +0300

    Delete src/main/java/tech/reliab/course/bahtin directory

--- src/main/java/tech/reliab/course/bahtin/bank/service/BankService.java
+++
@@ -1,9 +0,0 @@
-package tech.reliab.course.bahtin.bank.service;
-import tech.reliab.course.bahtin.bank.entity.Bank;
-
-public interface BankService {
-    Bank create(String name);
-    Bank read();
-    void update(Bank bank);
-    void delete(Bank bank);
-}
